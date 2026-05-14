12eba3e19bbe31dcfce65ff3deb8c47c0242d582
Chepi <chepisyahbudienbasil@gmail.com>
2026-05-15 01:05:51 +0700

    post login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\Api\ProfileController;
 
 // Auth
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return response()->json($request->user());
