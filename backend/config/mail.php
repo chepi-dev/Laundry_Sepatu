9fa1293eac9e4e3bd9fbae64c05851cb2aa86e10
Chepi <chepisyahbudienbasil@gmail.com>
2026-05-15 09:37:05 +0700

    mail

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -38,13 +38,14 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
+            'transport' => env('MAIL_TRANSPORT', 'smtp'),
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
